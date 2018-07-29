f36d52ea4cd5ea79e79689cc6d592c23aad47a21
ChrRubin <TP040985@mail.apu.edu.my>
2018-07-30 06:28:59 +0800

    add paddingBottom argument for responsive wrapper

--- index.js
+++ index.js
@@ -4,19 +4,23 @@
 * Gfycat tag
 *
 * Syntax:
-*   {% gfycat FunnyGfycatIdentifier fixed|responsive|js [width height] %}
+*   {% gfycat FunnyGfycatIdentifier fixed|responsive|js [width height]|[paddingBottom] %}
 */
 
 function gfycatTag(args, content) {
     var identifier = args[0];
     var wrapperType = args[1];
     var width = args[2];
+    var paddingBottom = args[2];
     var height = args[3];
 
     if(wrapperType === 'fixed') {
         return '<iframe src="https://gfycat.com/ifr/' + identifier + '" frameborder="0" scrolling="no" allowfullscreen width="' + width + '" height="' + height + '"></iframe>';
     } else if(wrapperType === 'responsive') {
-        return '<div style="position:relative;padding-bottom:180%"><iframe src="https://gfycat.com/ifr/' + identifier + '" frameborder="0" scrolling="no" width="100%" height="100%" style="position:absolute;top:0;left:0;" allowfullscreen></iframe></div>';
+        if(!paddingBottom) {
+            paddingBottom = "180%";
+        }
+        return '<div style="position:relative;padding-bottom:' + paddingBottom + '"><iframe src="https://gfycat.com/ifr/' + identifier + '" frameborder="0" scrolling="no" width="100%" height="100%" style="position:absolute;top:0;left:0;" allowfullscreen></iframe></div>';
     } else if (wrapperType === 'js') {
         return '<div class="gfyitem" data-id="' + identifier + '"></div>';
     } else {
